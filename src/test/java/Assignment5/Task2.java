c512ac51c08995334f7873a0e70e8f74fa725214
Jinal Patel <jinal.patel@sunlife.com>
2023-02-21 00:43:36 -0600

    Assignment 5

--- src/test/java/Assignment5/Task2.java
+++ src/test/java/Assignment5/Task2.java
@@ -1,5 +1,6 @@
 package Assignment5;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,14 @@ public class Task2 {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		boolean logoPresence = driver.findElement(By.xpath("//img[contains(@src,'/web/images/ohrm_branding.png')]")).isDisplayed();
+		
+		if(logoPresence){
+			System.out.println("Logo is present");
+			
+		} else{
+			System.out.println("Logo is not present");
+		}
 
 	}
 
--- src/test/java/Assignment5/Task4.java
+++ src/test/java/Assignment5/Task4.java
@@ -1,13 +1,48 @@
 package Assignment5;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task4 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		List<WebElement> smIcon = driver.findElements(By.xpath("//div[@class='orangehrm-login-footer-sm']/a"));
+		for(WebElement ele:smIcon){
+			if(ele.getAttribute("href").contains("linkedin")){
+				HashMap<String,String> smIconMap = new HashMap<String,String>();
+				smIconMap.put("Linkedin", ele.getAttribute("href"));
+				System.out.println(smIconMap);
+				
+
+			}else if (ele.getAttribute("href").contains("facebook")){
+				HashMap<String,String> smIconMap = new HashMap<String,String>();
+				smIconMap.put("FaceBook", ele.getAttribute("href"));
+				System.out.println(smIconMap);
+			
+			} else if(ele.getAttribute("href").contains("twitter")){
+				HashMap<String,String> smIconMap = new HashMap<String,String>();
+				smIconMap.put("Twitter", ele.getAttribute("href"));
+				System.out.println(smIconMap);
+			} else if(ele.getAttribute("href").contains("youtube")){
+				HashMap<String,String> smIconMap = new HashMap<String,String>();
+				smIconMap.put("YouTube", ele.getAttribute("href"));
+				System.out.println(smIconMap);
+
+			}
+		
+		
+		}
+		
 
 	}
 
