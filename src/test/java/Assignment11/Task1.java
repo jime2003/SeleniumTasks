240800fb30d9ed376481cff13500b66c707f1537
jinapate <jinapate@LNAR-5CG8517RR0.corp.capgemini.com>
2023-03-07 02:24:24 -0500

    Assignemnt 11 Submission

--- src/test/java/Assignment11/Task1.java
+++ src/test/java/Assignment11/Task1.java
@@ -1,6 +1,13 @@
 package Assignment11;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import Helpers.Utility;
@@ -8,8 +15,22 @@ import Helpers.Utility;
 public class Task1 {
     
     @Test
-    public void explicitWait(){
+    public void explicitWaitWithCondition1(){
         WebDriver driver = Utility.startBrowser("Chrome", "http://seleniumpractise.blogspot.com/2016/08/how-to-use-explicit-wait-in-selenium.html");
-        
+        driver.findElement(By.xpath("//button[text()='Click me to start timer']")).click();
+        WebElement ele = driver.findElement(By.xpath("//p[@id='demo']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.textToBe(By.xpath("//p[@id='demo']"), "WebDriver"));
+    
+    }
+    
+    @Test
+    public void explicitWaitWithCondition2(){
+        WebDriver driver = Utility.startBrowser("Chrome", "http://seleniumpractise.blogspot.com/2016/08/how-to-use-explicit-wait-in-selenium.html");
+        driver.findElement(By.xpath("//button[text()='Click me to start timer']")).click();
+        WebElement ele = driver.findElement(By.xpath("//p[@id='demo']"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.textToBePresentInElement(ele, "WebDriver"));
+    
     }
 }
--- src/test/java/Helpers/Utility.java
+++ src/test/java/Helpers/Utility.java
@@ -1,13 +1,21 @@
 package Helpers;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.io.FileHandler;
 import org.testng.Reporter;
 
 public class Utility {   
@@ -40,6 +48,63 @@ public class Utility {
 
             return driver;
         }
+        
+        public static void waitForSeconds(int seconds) {
+        	try {
+				Thread.sleep(seconds*1000);
+			} catch (InterruptedException e) {
+				
+			}
+        }
+        
+        public static WebElement waitForElement(WebElement element,int seconds) throws InterruptedException {
+        	int intMillis = 1000;
+        	boolean flag = false;
+        	long end = System.currentTimeMillis() + (seconds * intMillis);
+        	while(System.currentTimeMillis()<end) {
+        		try {
+        			element.isEnabled();
+        			flag=true;
+        			break;
+        			
+					
+				} catch (Exception e) {
+					 System.out.println("Still waiting for the element");
+					
+				}
+        			waitForSeconds(1);
+        	}
+        	return element;
+        }
+        
+        public static String getCurrentTime() {
+        	String date = new SimpleDateFormat("HH:mm:ss_dd_MM_yyyy").format(new Date());
+        	return date;
+     
+        }
+        
+        public static void captureScreenshotOfPage(WebElement element, WebDriver driver) {
+        	try {
+				FileHandler.copy(((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE), new File("./screenshots/screenshot"+getCurrentTime()+".png"));
+			} catch (IOException e) {
+				System.out.println("Something wen wrong"+e.getMessage());
+		
+			}
+        }
+        
+        public static void captureScreenshotOfWebElement(WebElement element, WebDriver driver) {
+        	try {
+      
+				FileHandler.copy(element.getScreenshotAs(OutputType.FILE), new File("./elementScreenshots/element"+getCurrentTime()+".png"));
+			} catch (IOException e) {
+				System.out.println("Something wen wrong"+e.getMessage());
+		
+			}
+        }
+        
+        
+        
+     
 }    
     
 
